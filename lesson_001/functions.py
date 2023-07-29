61619f28623ec8f6ce9d548831926a7f791699ed
Miia-Zapotichna <miia.zapotichna@gmail.com>
2023-07-29 11:42:23 +0200

    lesson 2

---
+++ lesson_001/functions.py
@@ -0,0 +1,28 @@
+def sum(num1, num2):
+    return num1 + num2
+
+
+print(sum(3, 4))
+
+print(sum("num1","num2"))
+
+def substract(num1, num2):
+    return num1 - num2
+
+
+print(substract(5, 8))
+
+
+def multiply(num1, num2):
+    return num1 * num2
+
+
+print(multiply(5, 2))
+
+
+def divide(num1, num2):
+    return num1 / num2
+
+
+print(divide(3, 4))
+
